5c97fba3801eaef9f4bd53da5c9a886b6433b436
eight <641137800@qq.com>
2025-12-09 22:15:06 +0800

    Fix build Issue， 无逻辑变化

--- backend/de.metas.externalsystem/src/test/java/de/metas/externalsystem/ExternalSystemTypeTest.java
+++ backend/de.metas.externalsystem/src/test/java/de/metas/externalsystem/ExternalSystemTypeTest.java
@@ -33,8 +33,6 @@ class ExternalSystemTypeTest
 	{
 		assertThat(ExternalSystemType.ofCodeOrNameOrNull("S6")).isEqualTo(ExternalSystemType.Shopware6);
 		assertThat(ExternalSystemType.ofCodeOrNameOrNull("Shopware6")).isEqualTo(ExternalSystemType.Shopware6);
-		assertThat(ExternalSystemType.ofCodeOrNameOrNull("A")).isEqualTo(ExternalSystemType.Alberta);
-		assertThat(ExternalSystemType.ofCodeOrNameOrNull("Alberta")).isEqualTo(ExternalSystemType.Alberta);
 		assertThat(ExternalSystemType.ofCodeOrNameOrNull("Ebay")).isEqualTo(ExternalSystemType.Ebay);
 		assertThat(ExternalSystemType.ofCodeOrNameOrNull("WOO")).isEqualTo(ExternalSystemType.WOO);
 		assertThat(ExternalSystemType.ofCodeOrNameOrNull("SAP")).isEqualTo(ExternalSystemType.SAP);
