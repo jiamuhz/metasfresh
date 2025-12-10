9416a0da1bafb493b9d51c81fcfb3837069c52d7
eight <641137800@qq.com>
2025-12-10 08:25:56 +0800

    移除支付项目： ESR 和 revolut

--- backend/de.metas.payment.esr/src/main/java-xjc_camt054_001_06/de/metas/payment/camt054_001_06/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// 此文件是由 Eclipse Implementation of JAXB v2.3.7 生成的
-// 请访问 https://eclipse-ee4j.github.io/jaxb-ri 
-// 在重新编译源模式时, 对此文件的所有修改都将丢失。
-// 生成时间: 2025.12.09 时间 04:11:19 PM CST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:iso:std:iso:20022:tech:xsd:camt.054.001.06", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package de.metas.payment.camt054_001_06;
