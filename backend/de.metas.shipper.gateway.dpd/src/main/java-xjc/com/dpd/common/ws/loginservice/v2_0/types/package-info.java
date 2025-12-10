80f08b3db426be78484f47de410281def433f7dc
eight <641137800@qq.com>
2025-12-10 08:19:00 +0800

    移除 运输公司： go 和 dpd

--- backend/de.metas.shipper.gateway.dpd/src/main/java-xjc/com/dpd/common/ws/loginservice/v2_0/types/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// 此文件是由 Eclipse Implementation of JAXB v2.3.7 生成的
-// 请访问 https://eclipse-ee4j.github.io/jaxb-ri 
-// 在重新编译源模式时, 对此文件的所有修改都将丢失。
-// 生成时间: 2025.12.09 时间 01:23:44 PM CST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://dpd.com/common/service/types/LoginService/2.0")
-package com.dpd.common.ws.loginservice.v2_0.types;
