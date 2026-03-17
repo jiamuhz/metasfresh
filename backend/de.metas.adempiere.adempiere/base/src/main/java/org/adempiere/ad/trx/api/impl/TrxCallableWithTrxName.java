4e500a54633ae4a70477d98a8fa74569b329ad24
eight <641137800@qq.com>
2026-03-17 22:28:27 +0800

    remove licenses

--- backend/de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/trx/api/impl/TrxCallableWithTrxName.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/trx/api/impl/TrxCallableWithTrxName.java
@@ -6,27 +6,7 @@ import org.adempiere.ad.trx.api.TrxCallable;
 import org.adempiere.ad.wrapper.IInterfaceWrapperHelper;
 import org.compiere.util.TrxRunnable;
 
-/*
- * #%L
- * de.metas.adempiere.adempiere.base
- * %%
- * Copyright (C) 2016 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
+/** */
 
 /**
  * Legacy adapter interface: it's used to adapt {@link TrxRunnable#run(String)} calls to {@link TrxCallableWithTrxName#call(String)} calls.
