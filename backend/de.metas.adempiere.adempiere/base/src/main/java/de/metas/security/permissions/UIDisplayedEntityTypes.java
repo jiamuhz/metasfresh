c131189c9130d52ed4e0fe0a96c689f94ca5b4e6
eight <641137800@qq.com>
2026-03-17 22:26:07 +0800

    remove licenses

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/security/permissions/UIDisplayedEntityTypes.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/security/permissions/UIDisplayedEntityTypes.java
@@ -8,27 +8,7 @@ import org.compiere.util.Env;
 import de.metas.security.IUserRolePermissions;
 import lombok.NonNull;
 
-/*
- * #%L
- * de.metas.adempiere.adempiere.base
- * %%
- * Copyright (C) 2015 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
+/** */
 
 /**
  * Constraint used to determine if the UI elements (windows, tabs, fields, menu, processes etc) shall be displayed in UI.
--- backend/de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/trx/processor/api/impl/FixedBatchTrxItemProcessor.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/ad/trx/processor/api/impl/FixedBatchTrxItemProcessor.java
@@ -6,27 +6,7 @@ import org.adempiere.ad.trx.processor.spi.ITrxItemProcessor;
 
 import de.metas.util.Check;
 
-/*
- * #%L
- * de.metas.adempiere.adempiere.base
- * %%
- * Copyright (C) 2015 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
+/** */
 
 /**
  * Wraps an {@link ITrxItemProcessor} and groups the items in batches of a given fixed maximum size.
