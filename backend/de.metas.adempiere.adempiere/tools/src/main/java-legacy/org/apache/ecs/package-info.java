ddfa3b5fa6351673082e87904945ec7239214560
eight <641137800@qq.com>
2026-03-18 20:22:47 +0800

    add  package-info.java

---
+++ backend/de.metas.adempiere.adempiere/tools/src/main/java-legacy/org/apache/ecs/package-info.java
@@ -0,0 +1,7 @@
+package org.apache.ecs;
+
+/**
+ * Apache ECS (Element Construction Set) 的 Java 库。
+ * 这个库的主要作用是通过 Java 对象来编程式地生成 HTML、XML 等标记语言文档。
+ * 它把每个标签都封装成 JavaBean，让你可以用面向对象的方式构建页面。
+ */
---
+++ backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/bankstatement_reconciliation/package-info.java
@@ -0,0 +1,6 @@
+package de.metas.ui.web.bankstatement_reconciliation;
+
+/**
+ * 银行对账
+ */
+
