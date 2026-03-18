47455af620436c2cfd30e188247fbad7e03f0e31
eight <641137800@qq.com>
2026-03-18 10:31:17 +0800

    just comments

--- backend/de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/facet/package-info.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/org/adempiere/facet/package-info.java
@@ -1,4 +1,7 @@
 /**
+ *
+ * 一种搜索方式
+ *
  * Core Facet functionality.
  * 
  * NOTE: if you want to learn more about facets, see Lucene Facets (e.g. http://lucene.apache.org/core/4_2_0/facet/org/apache/lucene/facet/doc-files/userguide.html )
