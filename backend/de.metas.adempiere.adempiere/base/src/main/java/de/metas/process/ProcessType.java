c767ac78159023365d3b7735c3347f0e561b9960
eight <641137800@qq.com>
2026-03-20 10:57:20 +0800

    just comment

--- backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/process/ProcessType.java
+++ backend/de.metas.adempiere.adempiere/base/src/main/java/de/metas/process/ProcessType.java
@@ -36,11 +36,34 @@ import org.compiere.model.X_AD_Process;
 @AllArgsConstructor
 public enum ProcessType implements ReferenceListAwareEnum
 {
+	/**
+	 * 执行一个 Java 类。你需要在进程定义中指定一个实现了特定接口（如 ProcessCall）的 Java 类的全名。系统会调用这个类的 doIt() 方法来执行业务逻辑。
+	 */
 	Java(X_AD_Process.TYPE_Java),
+
+	/**
+	 * 执行一条 SQL 语句。你需要在进程定义中提供一个 SELECT、UPDATE 或 DELETE 语句。系统会直接使用数据库连接来运行这段 SQL。
+	 */
 	SQL(X_AD_Process.TYPE_SQL),
+
+	/**
+	 * 调用一个 PostgREST 端点。是一个会触发对 PostgREST API 调用的操作。
+	 */
 	POSTGREST(X_AD_Process.TYPE_PostgREST),
+
+	/**
+	 * 使用 JSON 数据源生成 Jasper 报告。进程将运行一个 Jasper 报告，但报告所需的数据不是通过 SQL 查询，而是由一个 JSON 字符串（通常由后端准备）提供的。
+	 */
 	JasperReportsJSON(X_AD_Process.TYPE_JasperReportsJSON),
+
+	/**
+	 * 使用 SQL 数据源生成 Jasper 报告。这是最经典的方式。进程将运行一个 Jasper 报告，报告将使用进程定义中关联的 SQL 语句作为其数据源。
+	 */
 	JasperReportsSQL(X_AD_Process.TYPE_JasperReportsSQL),
+
+	/**
+	 * 生成一个 Excel 文件。这个进程将基于定义好的格式和查询，动态生成一个 Excel 报表并供用户下载。
+	 */
 	Excel(X_AD_Process.TYPE_Excel),
 	;
 
