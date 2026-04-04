19abcc9aaa36310080d7907c8fcb2413dcd8647e
eight <641137800@qq.com>
2026-04-04 20:44:40 +0800

    remove licenses

--- backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/window/descriptor/sql/SqlSelectDisplayValue.java
+++ backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/window/descriptor/sql/SqlSelectDisplayValue.java
@@ -15,27 +15,7 @@ import org.compiere.util.Evaluatee;
 import javax.annotation.Nullable;
 import java.util.Objects;
 
-/*
- * #%L
- * metasfresh-webui-api
- * %%
- * Copyright (C) 2020 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
+
 
 /**
  * 简单理解：当你的订单行存储了 C_BPartner_ID = 123，SqlSelectDisplayValue 负责生成类似这样的 SQL：
