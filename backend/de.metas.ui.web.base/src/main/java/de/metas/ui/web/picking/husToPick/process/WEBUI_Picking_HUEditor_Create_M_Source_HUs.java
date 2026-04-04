c76bcf21d6c5ff05c49569b23cb2888fb20e70c4
eight <641137800@qq.com>
2026-04-04 20:10:06 +0800

    remove licenses

--- backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/picking/husToPick/process/WEBUI_Picking_HUEditor_Create_M_Source_HUs.java
+++ backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/picking/husToPick/process/WEBUI_Picking_HUEditor_Create_M_Source_HUs.java
@@ -7,27 +7,7 @@ import de.metas.ui.web.picking.pickingslot.process.WEBUI_Picking_PickQtyToComput
 import de.metas.ui.web.picking.pickingslot.process.WEBUI_Picking_PickQtyToExistingHU;
 import de.metas.ui.web.picking.pickingslot.process.WEBUI_Picking_PickQtyToNewHU;
 
-/*
- * #%L
- * metasfresh-webui-api
- * %%
- * Copyright (C) 2017 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
+ 
 
 /**
  * This process is available from the HU editor window opened by {@link WEBUI_Picking_HUEditor_Launcher}.<br>
--- backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/picking/pickingslot/process/WEBUI_Picking_PickQtyToExistingHU.java
+++ backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/picking/pickingslot/process/WEBUI_Picking_PickQtyToExistingHU.java
@@ -31,27 +31,7 @@ import static de.metas.ui.web.picking.PickingConstants.MSG_WEBUI_PICKING_MISSING
 import static de.metas.ui.web.picking.PickingConstants.MSG_WEBUI_PICKING_NO_UNPROCESSED_RECORDS;
 import static de.metas.ui.web.picking.PickingConstants.MSG_WEBUI_PICKING_SELECT_PICKED_HU;
 
-/*
- * #%L
- * metasfresh-webui-api
- * %%
- * Copyright (C) 2017 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
+ 
 
 /**
  * 
