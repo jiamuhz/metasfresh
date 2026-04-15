35b2d510ed9f794cc5cb56f4d7dcdda8e00a390f
eight <641137800@qq.com>
2026-04-15 08:34:36 +0800

    remove licenses

--- backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/order/products_proposal/view/OrderProductsProposalViewFactory.java
+++ backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/order/products_proposal/view/OrderProductsProposalViewFactory.java
@@ -47,27 +47,6 @@ import static de.metas.ui.web.order.products_proposal.model.ProductsProposalRow.
 import static de.metas.ui.web.order.products_proposal.model.ProductsProposalRow.FIELD_QuotationOrdered;
 import static de.metas.ui.web.order.products_proposal.model.ProductsProposalRow.FIELD_TermsOfDelivery;
 
-/*
- * #%L
- * metasfresh-webui-api
- * %%
- * Copyright (C) 2019 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
 
 @ViewFactory(windowId = OrderProductsProposalViewFactory.WINDOW_ID_STRING)
 public class OrderProductsProposalViewFactory extends ProductsProposalViewFactoryTemplate
--- backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/payment_allocation/PaymentRows.java
+++ backend/de.metas.ui.web.base/src/main/java/de/metas/ui/web/payment_allocation/PaymentRows.java
@@ -17,27 +17,6 @@ import org.compiere.model.I_C_Payment;
 import java.time.ZonedDateTime;
 import java.util.List;
 
-/*
- * #%L
- * metasfresh-webui-api
- * %%
- * Copyright (C) 2019 metas GmbH
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program. If not, see
- * <http://www.gnu.org/licenses/gpl-2.0.html>.
- * #L%
- */
 
 /**
  * @implNote Problem: the right table (invoices) is an includedView of the main table row (payments - left table).
